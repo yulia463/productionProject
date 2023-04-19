a459fa3aef7ee7617cecac93386da79d32e799a2
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-04-19 14:13:44 +0200

    Add new methodology feature-sliced

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,6 +1,5 @@
 import {BuildOptions} from "./types/config";
 import webpack from "webpack";
-import path from "path";
 import {buildPlugins} from "./buildPlugins";
 import {buildLoaders} from "./buildLoaders";
 import {buildResolvers} from "./buildResolvers";
@@ -10,7 +9,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
     const {paths, mode, isDev} = options;
 
     return {
-        mode: mode,
+        mode,
         entry: paths.entry,
         output: {
             filename: "[name].[contenthash].js",
@@ -21,7 +20,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
         module: {
             rules: buildLoaders(options),
         },
-        resolve: buildResolvers(),
+        resolve: buildResolvers(options),
         devtool: isDev ? 'inline-source-map' : undefined,
         devServer: isDev ? buildDevServer(options) : undefined,
     }
