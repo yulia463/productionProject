76ded543419299a8a211eb2e1f6366a2e579ca12
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-05-29 11:34:07 +0200

    feat : fixed errors

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'app/providers/ThemeProviders';
+import { StoreProvider } from 'app/providers/StoreProvider';
+import App from './app/App';
+import 'app/styles/index.scss';
+import 'shared/config/i18n/i18n';
+
+render(
+    <StoreProvider>
+        <BrowserRouter>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </BrowserRouter>
+    </StoreProvider>,
+    document.getElementById('root'),
+);
